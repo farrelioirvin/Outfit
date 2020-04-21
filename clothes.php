dd4a93341ef1f754ffe9214d6416e483a9a4386d
farrelioirvin <60589694+farrelioirvin@users.noreply.github.com>
2020-04-21 17:29:00 +0700

    Add files via upload

---
+++ clothes.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="fontawesome-free/css/all.min.css">
+    <title>Head</title>
+  </head>
+  <body>
+    
+  <h3 class="bg-warning text-center font-weight-bold p-3">INI ADALAH PRODUK YANG TERSEDIA UNTUK BAGIAN KEPALA</h3>
+    <div class="container">
+    
+      <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head1.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1">erdgfhj</h5>
+            <p class="card-text m-1">Zcdvf</p>
+            <a href="" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head2.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1">XCv</h5>
+            <p class="card-text m-1">zxvbc</p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head3.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head4.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head5.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head6.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head7.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head8.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head9.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="#" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        <div class="row">
+        <div class="col-md-3">
+         <div class="card" >
+          <img src="head10.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+            <h5 class="card-title m-1"></h5>
+            <p class="card-text m-1"></p>
+            <a href="" class="btn btn-primary"></a>
+          </div>
+         </div>
+        </div>
+        
+      </div>
+   </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="fontawesome-free/css/all.min.css">
+    <link rel="icon" type="image/png" href="goat.png"/>
+    <title>THE GOATZ</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+    <div class="container">
+    <h3><i class="fas fa-shopping-cart mr-2"></i></h3>
+    <a class="navbar-brand font-weight-bold" href="#">THE GOATZ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      
+        <ul class="navbar-nav ml-auto mr-4">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="catalog.php">Catalog <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="about.html">About <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="colorlib-regform-8/register.php">Register <span class="sr-only">(current)</span></a>
+          </li>
+          
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+      </div>
+    </nav>
+    <div class="row mt-5 no-gutters">
+      <div class="col-md-2 bg-light">
+        <ul class="list-group list-group-flush p-2 pt-4">
+          <li class="list-group-item bg-warning"><i class="fas fa-list mr-2"></i>PRODUCT CATEGORY</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> <a href="head.php">Head </a></li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> <a href="clothes.php">Clothes</a></li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> <a href="#">Pants</a></li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> <a href="#">Shoes</a></li>
+        </ul>
+      </div>
+      <div class="col-md-10">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" >
+          <div class="carousel-item active">
+            <img src="outfit4.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="outfit6.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="outfit5.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+        </a>
+      </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
